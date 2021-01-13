066b2980bb27e23f7c6762acf70d8a45239e6b23
Filippo Maria Briscese <24792631+FiloBriscese@users.noreply.github.com>
2021-01-13 19:58:26 +0100

    TravisCI set-up

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+// TODO delete this class after TravisCI and SonarCloud setup.
+
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
